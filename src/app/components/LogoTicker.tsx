e14a6c6853b24d02eae6ef5b6fbfadadb3784f74
kizito3 <chigokizzy@gmail.com>
2024-07-13 13:51:25 +0100

    final build2

--- src/app/components/LogoTicker.tsx
+++ src/app/components/LogoTicker.tsx
@@ -7,7 +7,7 @@ import echo from "../assets/echo.png";
 import celestia from "../assets/celestia.png";
 import pulse from "../assets/pulse.png";
 import apex from "../assets/apex.png";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 
 const images = [
   { src: acme, alt: "acme logo" },
@@ -22,17 +22,20 @@ export const LogoTicker = () => {
   return (
     <div className="bg-black text-white py-[72px] sm:py-24">
       <div className="container">
-        <h2 className="text-xl text-center text-white/70">Trusted by the world's most innovative teams</h2>
-        <div className="flex overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-team-gradient after:bg-team-gradient before:z-10">
-          <motion.div 
-          initial={{ translateX: 0}}
-          animate={{ translateX: "-50%"}}
-          transition={{
-            duration: 10,
-            ease: "linear",
-            repeat: Infinity,
-          }}
-          className="flex gap-20 flex-none sm:justify-center pr-20">
+        <h2 className="text-xl text-center text-white/70">
+          Trusted by the world&#39;s most innovative teams
+        </h2>
+        <div className="flex overflow-hidden mt-9 before:content-[&#39;&#39;] after:content-[&#39;&#39;] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-team-gradient after:bg-team-gradient before:z-10">
+          <motion.div
+            initial={{ translateX: 0 }}
+            animate={{ translateX: "-50%" }}
+            transition={{
+              duration: 10,
+              ease: "linear",
+              repeat: Infinity,
+            }}
+            className="flex gap-20 flex-none sm:justify-center pr-20"
+          >
             {images.map(({ src, alt }, index) => (
               <Image
                 key={index}
@@ -41,7 +44,7 @@ export const LogoTicker = () => {
                 className="flex-none h-8 w-auto"
               />
             ))}
-             {images.map(({ src, alt }, index) => (
+            {images.map(({ src, alt }, index) => (
               <Image
                 key={index}
                 src={src}
